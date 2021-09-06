e6e0995286525ab7da6e3badc37f63325544a5e4
unknown <wendosam21@gmail.com>
2021-09-06 10:48:27 +0300

    alterd tables in db

--- laravel/myblog/app/Models/post.php
+++ laravel/myblog/app/Models/post.php
@@ -1,31 +1,11 @@
 <?php
+
 namespace App\Models;
 
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post {
-
-    public static function all(){
-
-   $files = File::files(resource_path("posts/"));
-
-   $posts=  collect($files)->map(
-       fn($file) =>
-          YamlFrontMatter::parseFile
-          ($file)
-     );
-
-     return $posts;
-
-    }
-
-    public static function find($id){
-
-        $allPosts = static::all();
-        $post = $allPosts->filter(
-            fn($post)=> $post->id == $id
-        );
-            return $post[0];
-    }
+class Post extends Model
+{
+    use HasFactory;
 }
